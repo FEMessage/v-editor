dad2b82e40f9e70eccaffbc3e9d28797a537126f
colmugx <colmugx@qq.com>
2020-01-09 15:58:17 +0800

    fix: 直接调用 menu 菜单

--- src/mdParser.js
+++ src/mdParser.js
@@ -1,11 +1,21 @@
-const SPACE = '[&nbsp;|\\s]'
+const SPACE = '(?:&nbsp;|\\s)'
 
 const HEAD = `(#){1,4}${SPACE}`
 const UL = [`-${SPACE}`, 'insertUnorderedList']
 const OL = [`\\d\\.${SPACE}`, 'insertOrderedList']
 
-const p = symbol => `\\<[p|div]\\>${symbol}(.*?)\\<\\/[p|div]\\>`
+const p = symbol => `\\<(?:div|p)\\>${symbol}(.*?)\\<\\/(?:div|p)\\>`
 
+/**
+ * markdown 语法转换
+ *
+ * 该版本有两个问题：
+ * 1. 如果要清空 md 语法文本，格式会在最后一行生效
+ * 2. 如果要在中间插入文本，md 语法文本无法清空
+ *
+ * @param {String} html onChange 文本
+ * @param {Object} editor 编辑器实例
+ */
 const mdParser = (html, editor) => {
   if (RegExp(p(HEAD)).test(html)) {
     const reg = RegExp(p(HEAD))
@@ -17,22 +27,19 @@ const mdParser = (html, editor) => {
     match[0].replace(/#{1,}/g, text => (size = text.length))
 
     editor.txt.html(
-      html.replace(RegExp(`\\<div\\>#{1,4}${SPACE}*`), `<div><br>`)
+      html.replace(RegExp(`\\<(div|p)\\>#{1,4}${SPACE}*`), `<$1><br>`)
     )
-    editor.txt.html(html.replace(RegExp(`\\<p\\>#{1,4}${SPACE}*`), `<p><br>`))
-    editor.cmd.do('formatBlock', `<h${size}>`)
+    editor.menus.menus.head._command(`h${size}`)
   } else if (RegExp(p(UL[0])).test(html)) {
     editor.txt.html(
-      html.replace(RegExp(`\\<div\\>${UL[0]}${SPACE}*`), `<div><br>`)
+      html.replace(RegExp(`\\<(div|p)\\>${UL[0]}${SPACE}*`), `<$1><br>`)
     )
-    editor.txt.html(html.replace(RegExp(`\\<p\\>${UL[0]}${SPACE}*`), `<p><br>`))
-    editor.cmd.do(UL[1])
+    editor.menus.menus.list._command(UL[1])
   } else if (RegExp(p(OL[0])).test(html)) {
     editor.txt.html(
-      html.replace(RegExp(`\\<div\\>${OL[0]}${SPACE}*`), `<div><br>`)
+      html.replace(RegExp(`\\<(div|p)\\>${OL[0]}${SPACE}*`), `<$1><br>`)
     )
-    editor.txt.html(html.replace(RegExp(`\\<p\\>${OL[0]}${SPACE}*`), `<p><br>`))
-    editor.cmd.do(OL[1])
+    editor.menus.menus.list._command(OL[1])
   }
 }
 
