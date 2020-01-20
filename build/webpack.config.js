c21a184ac158bdfd616234fcb5e1d08e7ff90473
ColMugX <colmugx@qq.com>
2020-01-20 15:49:34 +0800

    feat!: 更换编辑器内核，更好地支持 markdown (#60)

---
+++ build/webpack.config.js
@@ -0,0 +1,85 @@
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin')
+const {bundler, styles} = require('@ckeditor/ckeditor5-dev-utils')
+const {VueLoaderPlugin} = require('vue-loader')
+const TerserPlugin = require('terser-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: [
+    require.resolve('regenerator-runtime/runtime.js'),
+    path.resolve('src', 'index.js')
+  ],
+  output: {
+    library: 'VEditor',
+    path: path.resolve('dist'),
+    filename: 'v-editor.umd.js',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+  optimization: {
+    usedExports: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: /^!/
+          }
+        },
+        extractComments: false
+      })
+    ]
+  },
+  plugins: [
+    new CKEditorWebpackPlugin({
+      language: 'zh-cn'
+    }),
+    new webpack.BannerPlugin({
+      banner: bundler.getLicenseBanner(),
+      raw: true
+    }),
+    new VueLoaderPlugin()
+  ],
+  externals: ['@femessage/upload-to-ali'],
+  module: {
+    rules: [
+      {
+        test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+        use: ['raw-loader']
+      },
+      {
+        test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              injectType: 'singletonStyleTag'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: styles.getPostCssConfig({
+              themeImporter: {
+                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+              },
+              minify: true
+            })
+          }
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        loaders: ['vue-style-loader', 'css-loader', 'less-loader']
+      }
+    ]
+  }
+}
--- src/defaultEditorOptions.js
+++ src/defaultEditorOptions.js
@@ -1,24 +1,107 @@
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave'
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder'
+
+import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import Font from '@ckeditor/ckeditor5-font/src/font'
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline'
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline'
+
+import Image from '@ckeditor/ckeditor5-image/src/image'
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption'
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle'
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar'
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload'
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize'
+
+import Indent from '@ckeditor/ckeditor5-indent/src/indent'
+import Link from '@ckeditor/ckeditor5-link/src/link'
+import List from '@ckeditor/ckeditor5-list/src/list'
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist'
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed'
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
+import Table from '@ckeditor/ckeditor5-table/src/table'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
+
+// 本地插件
+import FullScreen from './plugin/FullScreen'
+import ExtraFormat from './plugin/ExtraFormat'
+
 export default {
-  debug: false,
-  onchangeTimeout: 200,
-  menus: [
-    'head',
-    'bold',
-    'fontSize',
-    'fontName',
-    'italic',
-    'underline',
-    'strikeThrough',
-    'foreColor',
-    'backColor',
-    'link',
-    'list',
-    'justify',
-    'quote',
-    'image',
-    'table',
-    'code',
+  plugins: [
+    Essentials,
+    Autosave,
+    Autoformat,
+    Font,
+    Bold,
+    Italic,
+    Strikethrough,
+    Underline,
+    BlockQuote,
+    HorizontalLine,
+    CKFinder,
+    Heading,
+    Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    ImageResize,
+    Indent,
+    Link,
+    List,
+    TodoList,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    Table,
+    TableToolbar,
+    FullScreen,
+    ExtraFormat
+  ],
+  toolbar: [
     'undo',
-    'redo'
-  ]
+    'redo',
+    '|',
+    'heading',
+    'fontSize',
+    '|',
+    'bold',
+    'italic',
+    'strikethrough',
+    'underline',
+    '|',
+    'fontColor',
+    'fontBackgroundColor',
+    '|',
+    'bulletedList',
+    'numberedList',
+    'todoList',
+    '|',
+    'link',
+    'blockQuote',
+    'horizontalLine',
+    '|',
+    'imageUpload',
+    'insertTable',
+    'mediaEmbed',
+    '|',
+    'fullScreen'
+  ],
+  fontSize: {
+    options: [10, 12, 14, 'default', 18, 22, 24]
+  },
+  image: {
+    resizeUnit: 'px',
+    toolbar: ['imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative']
+  },
+  table: {
+    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+  }
 }
