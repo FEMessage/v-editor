0d0199f09426335b86539f3538f0215c8dfae23b
colmugx <colmugx@qq.com>
2020-02-03 16:33:41 +0800

    style: 全屏缩小图标

--- src/defaultEditorOptions.js
+++ src/defaultEditorOptions.js
@@ -94,6 +94,39 @@ export default {
     '|',
     'fullScreen'
   ],
+  heading: {
+    options: [
+      {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+      {
+        model: 'heading1',
+        view: 'h1',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1'
+      },
+      {
+        model: 'heading2',
+        view: 'h2',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2'
+      },
+      {
+        model: 'heading3',
+        view: 'h3',
+        title: 'Heading 3',
+        class: 'ck-heading_heading3'
+      },
+      {
+        model: 'heading4',
+        view: 'h4',
+        title: 'Heading 4'
+      },
+      {
+        model: 'heading5',
+        view: 'h5',
+        title: 'Heading 5'
+      }
+    ]
+  },
   fontSize: {
     options: [10, 12, 14, 'default', 18, 22, 24]
   },
