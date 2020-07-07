7f669378fef8944e70f3ccd9146336c43c7131c4
snowlocked <wuzanh@gmail.com>
2020-07-07 17:48:56 +0800

    fix: upload失败时的处理

--- src/utils/adapter.js
+++ src/utils/adapter.js
@@ -6,17 +6,24 @@ export class UploadAdapter {
   }
 
   async upload() {
-    try {
+    const file = await this.loader.file
+    return new Promise((resolve, reject) => {
       // 图片多选时会逐个调用此方法
-      const file = await this.loader.file
-      const url = await this.uploadImg(file)
-
-      return {
-        default: url
-      }
-    } catch (error) {
-      console.error(error)
-    }
+      this.uploadImg(file)
+        .then(url => {
+          // 没有url意味着上传没有执行，需要reject
+          if (url) {
+            resolve({
+              default: url
+            })
+          } else {
+            reject()
+          }
+        })
+        .catch(e => {
+          reject(e)
+        })
+    })
   }
 
   abort() {}
--- src/v-editor.vue
+++ src/v-editor.vue
@@ -135,7 +135,12 @@ export default {
       this.$emit('upload-start')
       request
         .then(res => {
-          this.$emit('upload-end', true, res)
+          // res没有返回意味着上传过程中发现upload文件大小超出限制或其他不能上传的限制导致上传不能执行
+          if (res) {
+            this.$emit('upload-end', true, res)
+          } else {
+            this.$emit('upload-end', false, 'fail')
+          }
         })
         .catch(e => {
           this.$emit('upload-end', false, e)
