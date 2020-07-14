3d5fb0eb56de8475f580bdd7099b446c1d0db69c
snowlocked <wuzanh@gmail.com>
2020-07-14 11:28:18 +0800

    feat: 修改插件生成方式

--- src/plugin/Essentials.js
+++ src/plugin/Essentials.js
@@ -1,34 +1,14 @@
-/**
- * @module essentials/essentials
- */
-
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin'
-
+import generateUIPlugin from './generateUIPlugin'
 import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard'
 import Enter from '@ckeditor/ckeditor5-enter/src/enter'
 import ShiftEnter from '@ckeditor/ckeditor5-enter/src/shiftenter'
 import Typing from '@ckeditor/ckeditor5-typing/src/typing'
-import Undo from './undo'
+import Undo from './Undo'
 
-/**
- * A plugin including all essential editing features. It represents a set of features that enables similar functionalities
- * to a `<textarea>` element.
- * If your editor is supposed to handle block content, make sure to include it.
- *
- * @extends module:core/plugin~Plugin
- */
-export default class Essentials extends Plugin {
-  /**
-   * @inheritDoc
-   */
-  static get requires() {
-    return [Clipboard, Enter, ShiftEnter, Typing, Undo]
-  }
-
-  /**
-   * @inheritDoc
-   */
-  static get pluginName() {
-    return 'Essentials'
-  }
-}
+export default generateUIPlugin('Essentials', [
+  Clipboard,
+  Enter,
+  ShiftEnter,
+  Typing,
+  Undo
+])
