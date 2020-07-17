4bb3140e07cf5103da18b1ddde183e1926f902ba
snowlocked <wuzanh@gmail.com>
2020-07-17 11:12:09 +0800

    feat: 修改引用

--- src/plugin/Mediaembed/AutoMediaEmbed.js
+++ src/plugin/Mediaembed/AutoMediaEmbed.js
@@ -1,7 +1,7 @@
 import AutoMediaEmbed from '@ckeditor/ckeditor5-media-embed/src/automediaembed'
 import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard'
 
-import Undo from '../Undo1'
+import Undo from '../Undo'
 
 export default class AutoMediaEmbedCustom extends AutoMediaEmbed {
   static get requires() {
